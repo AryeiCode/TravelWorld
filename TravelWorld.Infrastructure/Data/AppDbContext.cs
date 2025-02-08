ddfdbfeb5cb24e7f9c55cc4cb0a262bc07ebddb2
Yeison Ariel Zaraza Molina <yeison.zaraza@globalmvm.com>
2025-02-08 16:40:42 -0500

    Se agrega DbSet y se pobla la tabla CountryHouse

--- TravelWorld.Infrastructure/Data/AppDbContext.cs
+++ TravelWorld.Infrastructure/Data/AppDbContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelWorld.Domain.Entities;
 
 namespace TravelWorld.Infrastructure.Data
 {
@@ -11,6 +12,44 @@ namespace TravelWorld.Infrastructure.Data
     {
         public AppDbContext(DbContextOptions options) : base(options)
         {
+        }
+
+        public DbSet<CountryHouse> CountryHouses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CountryHouse>().HasData(
+            new CountryHouse
+            {
+                Id = 1,
+                Name = "Royal CountryHouse",
+                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                ImageUrl = "https://placehold.co/600x400",
+                Occupancy = 4,
+                Price = 200,
+                Sqft = 550,
+            },
+            new CountryHouse
+            {
+                Id = 2,
+                Name = "Premium Pool CountryHouse",
+                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                ImageUrl = "https://placehold.co/600x401",
+                Occupancy = 4,
+                Price = 300,
+                Sqft = 550,
+            },
+            new CountryHouse
+            {
+                Id = 3,
+                Name = "Luxury Pool CountryHouse",
+                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                ImageUrl = "https://placehold.co/600x402",
+                Occupancy = 4,
+                Price = 400,
+                Sqft = 750,
+            });
         }
     }
 }
---
+++ TravelWorld.Infrastructure/Migrations/20250208213755_seedAddCountryHouse.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TravelWorld.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedAddCountryHouse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CountryHouses",
+                columns: new[] { "Id", "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[,]
+                {
+                    { 1, null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", "Royal CountryHouse", 4, 200.0, 550.0, null },
+                    { 2, null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x401", "Premium Pool CountryHouse", 4, 300.0, 550.0, null },
+                    { 3, null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x402", "Luxury Pool CountryHouse", 4, 400.0, 750.0, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CountryHouses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CountryHouses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CountryHouses",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
