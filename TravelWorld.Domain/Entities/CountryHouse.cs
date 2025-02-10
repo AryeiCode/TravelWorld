ecdf017f77eeb45b2d4f506fb3b494df1c77a959
Yeison Ariel Zaraza Molina <yeison.zaraza@globalmvm.com>
2025-02-10 14:02:48 -0500

    Crud completo de CountryHouse.

--- TravelWorld.Domain/Entities/CountryHouse.cs
+++ TravelWorld.Domain/Entities/CountryHouse.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,16 @@ namespace TravelWorld.Domain.Entities
 {
     public class CountryHouse
     {
+        [Key]
         public int Id { get; set; }
+        [DisplayName("Nombre")]
+        [MaxLength(50)]
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Range(10, 10000)]
         public double Price { get; set; }
         public double Sqft { get; set; } // Propiedad metros cuadrados
+        [Range(1,10)]
         public int Occupancy { get; set; }
         public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
---
+++ TravelWorld.Web/Controllers/CountryHouseController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelWorld.Domain.Entities;
+using TravelWorld.Infrastructure.Data;
+
+namespace TravelWorld.Web.Controllers
+{
+    public class CountryHouseController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CountryHouseController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var countryHouse = _db.CountryHouses.ToList();
+            return View(countryHouse);
+        }
+
+        /*combinación de las palabras “update” (actualizar) e “insert” (insertar)*/
+        public IActionResult Upsert(int countryHouseId)
+        {
+            if (countryHouseId != 0)
+            {
+                var obj = _db.CountryHouses.FirstOrDefault(x => x.Id == countryHouseId);
+                return View(obj);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Upsert(CountryHouse obj)
+        {
+            /*if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("description", "No puede existir una descripción igual al nombre");
+            }*/
+            var error = ModelState.Values;
+
+
+            if (ModelState.IsValid)
+             {
+                if (obj.Id == 0)
+                {
+                    _db.CountryHouses.Add(obj);
+                    _db.SaveChanges();
+                }
+                else
+                {
+                    _db.CountryHouses.Update(obj);
+                    _db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //TODO: Refrescar la tabla cuando se elimina
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Delete(int countryHouseId, bool res)
+        {
+            var obj = _db.CountryHouses.FirstOrDefault(x => x.Id == countryHouseId);
+            if (obj != null && res)
+            {              
+                _db.CountryHouses.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
